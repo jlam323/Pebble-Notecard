cc33db95b2c4d3134ebcb9320996b560bc3badb2
Charlie Zhao <wzhao67@uwo.ca>
2015-11-28 12:26:00 -0500

    first upload

---
+++ build/c4che/_cache.py
@@ -0,0 +1,6 @@
+BINDIR = '/usr/local/bin'
+DEFINES = ['RELEASE']
+LIBDIR = '/usr/local/lib'
+PEBBLE_SDK_COMMON = '/home/charlie/pebble-dev/PebbleSDK-3.7/Pebble/common'
+PREFIX = '/usr/local'
+TARGET_PLATFORMS = [u'chalk', u'basalt', u'aplite']
---
+++ build/chalk/appinfo.auto.c
@@ -0,0 +1,20 @@
+#include "pebble_process_info.h"
+#include "src/resource_ids.auto.h"
+
+const PebbleProcessInfo __pbl_app_info __attribute__ ((section (".pbl_header"))) = {
+  .header = "PBLAPP",
+  .struct_version = { PROCESS_INFO_CURRENT_STRUCT_VERSION_MAJOR, PROCESS_INFO_CURRENT_STRUCT_VERSION_MINOR },
+  .sdk_version = { PROCESS_INFO_CURRENT_SDK_VERSION_MAJOR, PROCESS_INFO_CURRENT_SDK_VERSION_MINOR },
+  .process_version = { 1, 0 },
+  .load_size = 0xb6b6,
+  .offset = 0xb6b6b6b6,
+  .crc = 0xb6b6b6b6,
+  .name = "hello-pebble",
+  .company = "MakeAwesomeHappen",
+  .icon_resource_id = DEFAULT_MENU_ICON,
+  .sym_table_addr = 0xA7A7A7A7,
+  .flags = 0,
+  .num_reloc_entries = 0xdeadcafe,
+  .uuid = { 0x8A, 0xE6, 0x36, 0xC0, 0x8C, 0x29, 0x46, 0xED, 0xA8, 0x9A, 0xC8, 0xE1, 0xD5, 0xC5, 0xE3, 0xC0 },
+  .virtual_size = 0xb6b6
+};
